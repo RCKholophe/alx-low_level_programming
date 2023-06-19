391a8bb67e51cbbf3e81b592151570dd423dcad5
RCKholophe <khongelani8@gmail.com>
2023-06-19 00:38:55 -0700

    5-printf.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a newliner, to standard error
- *
- * Return: Always 0 (success)
+ * main -prints "and that piece of art is useful" - Dora Korpar, 2015-10-19" ,
+ * followed by a new line, to the standard error.
+ * Return: Always 1.
  */
 int main(void)
-
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"))
+
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n" ,59);
 	return (1);
 }
