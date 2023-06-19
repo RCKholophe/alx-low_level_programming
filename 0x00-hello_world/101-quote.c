893c63e08c827e7f6ca322f071db31678416588e
RCKholophe <khongelani8@gmail.com>
2023-06-19 01:52:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 
 /**
- * Main - Entry point
+ * Main - /prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
  * Return: Always 1
  */
 
