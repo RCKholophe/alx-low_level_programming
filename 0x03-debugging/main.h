d158c48d93f7236dce3a99b516b3c2fafcc19569
RCKholophe <khongelani8@gmail.com>
2023-06-22 11:50:22 -0700

    debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIH_H
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int days, int year);
+int covert_day(int month,int day);
+
+#endif
+~       
