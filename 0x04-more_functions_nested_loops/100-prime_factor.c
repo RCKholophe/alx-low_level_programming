347d37f7a78ccb081604e0589d118c3d8be7e99f
RCKholophe <khongelani8@gmail.com>
2023-06-22 20:35:15 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <math.h>
+/**
+ * main - find and prints the largest factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	long int n;
+	long int max;
+	long int i;
+	
+	n= 612852475143
+	max = -1
+
+
