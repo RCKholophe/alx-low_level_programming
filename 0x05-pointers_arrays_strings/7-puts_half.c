8167b908e2a7bc5512662c18d68bd713c1c07034
RCKholophe <khongelani8@gmail.com>
2023-06-26 10:04:01 -0700

    vi 6-puts2.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,28 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
-int _strlen(char *s)
+void puts2(char *str)
 {
-	int longi = 0;
-
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
+        int longi = 0;
+        int t = 0;
+        char *y = str;
+        int o;
+       while (*y != '\0')
+        {
+                y++;
+                longi++;
+        }
+        t = longi - 1;
+        for (o = 0; o <= t ; o++)
 	}
-
-	return (longi);
+               if (o % 2 == 0)
+	}
+		_putchar(str[o]);
+	{
+	{
+	_putchar('\n');
 }
