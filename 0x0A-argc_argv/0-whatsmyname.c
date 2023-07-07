0d4f7cf4b15f10b10849333573e1f306c887351c
RCKholophe <khongelani8@gmail.com>
2023-07-07 10:30:03 -0700

    header file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - prints the name if the program
- *
+ * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv[0]);
-	(void)argc;
-	return (0);
+printf("%s\n", *argv);
+return (0);
 }
--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -6,7 +6,7 @@
  * @c: the charater to print
  *
  * Return: on success 1.
- * On error, -1 is returned, and error is set appropriately
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {
