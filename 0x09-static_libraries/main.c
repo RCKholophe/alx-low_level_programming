00105d6e0c676c5a8eac8e40731bb53a8df48812
RCKholophe <khongelani8@gmail.com>
2023-07-08 14:20:18 -0700

    my task

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
+	return (0);
+
+}
